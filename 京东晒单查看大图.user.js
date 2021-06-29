e2aab5acd4c2f637c30ecf91112cc70a0646851d
ssyatelandisi <ssyatelandisi@163.com>
2021-06-29 20:08:14 +0800

    v0.4
    v0.4

--- 京东晒单查看大图.user.js
+++ 京东晒单查看大图.user.js
@@ -1,73 +1,51 @@
 // ==UserScript==
 // @name         京东晒单查看大图
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @description  给京东评晒单及评价中的图片添加一个“查看大图”的按钮
 // @author       ssyatelandisi
 // @match        https://*.jd.com/*
-// @grant        none
+// @require      https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js
+// @grant        GM_addStyle
+/* global        $ */
 // ==/UserScript==
-
-(function() {
-    'use strict';
+(function () {
+    "use strict";
+    // @ts-ignore
+    GM_addStyle(`#openNewImg,#openNewImg2{color:#288cf0;border:2px #288cf0 solid;background-color:rgba(255,255,255,0.75);border-radius:5px;padding:5px;cursor:pointer;position:absolute;top:0;right:0;bottom:auto;left:auto;z-index:999;font-weight:normal}#openNewImg:hover,#openNewImg2:hover{color:#ffffff;border-color:#288cf0;background-color:#288cf0;font-weight:bolder}`);
     //查看晒单大图
-    $("body").delegate(".photo-wrap", "mouseenter", function () {
-        var that = this;
-        $(".photo-wrap").prepend('<button id="openNewImg" style="color:#288CF0;border:2px #288CF0 solid;background-color:rgba(255, 255, 255, .75);border-radius:5px;padding:5px;position:absolute;top:0;right:0;z-index:999"><b>查看大图</b></button>');
-        $("button#openNewImg").hover(function () {
-            $(this).css({
-                "color": "#FFFFFF",
-                "border-color": "#288CF0",
-                "background-color": "#288CF0",
-                "font-weight": "bolder"
-            });
-        }, function () {
-            $(this).css({
-                "color": "#288CF0",
-                "border-color": "#288CF0",
-                "background-color": "rgba(255, 255, 255, .75)",
-                "font-weight": "normal"
-            });
-        });
-        $("button#openNewImg").click(function () {
-            var regex1 = /n\d{1,2}/;
-            var regex2 = /s\d+x\d+_/;
-            var imgLink = $(that).find(".J-photo-img")[0].src;
-            var newImgLink = imgLink.replace(regex1, "shaidan");
+    $("body").on("mouseenter", ".photo-wrap", function () {
+        const that = this;
+        $(".photo-wrap").prepend('<button id="openNewImg"><b>查看大图</b></button>');
+        $("button#openNewImg").on("click", function () {
+            const regex1 = /n\d{1,2}/;
+            const regex2 = /s\d+x\d+_/;
+            const imgLink = $(that).find(".J-photo-img")[0].src;
+            let newImgLink = imgLink.replace(regex1, "shaidan");
             newImgLink = newImgLink.replace(regex2, "");
             window.open(newImgLink);
-        })
+            return false;
+        });
+        return false;
     });
-    $("body").delegate(".photo-wrap", "mouseleave", function () {
+    $("body").on("mouseleave", ".photo-wrap", function () {
         $("button#openNewImg").remove();
+        return false;
     });
     //查看评论大图
-    $("body").delegate(".pic-view.J-pic-view", "mouseenter", function () {
-        var that = this;
-        $(".pic-view.J-pic-view").prepend('<button id="openNewImg2" style="color:#288CF0;border:2px #288CF0 solid;background-color:rgba(255, 255, 255, .75);border-radius:5px;padding:5px;position:absolute;top:0;right:0;z-index:999"><b>查看大图</b></button>');
-        $("button#openNewImg2").hover(function () {
-            $(this).css({
-                "color": "#FFFFFF",
-                "border-color": "#288CF0",
-                "background-color": "#288CF0",
-                "font-weight": "bolder"
-            });
-        }, function () {
-            $(this).css({
-                "color": "#288CF0",
-                "border-color": "#288CF0",
-                "background-color": "rgba(255, 255, 255, .75)",
-                "font-weight": "normal"
-            });
-        });
-        $("button#openNewImg2").click(function () {
-            var regex2 = /s\d+x\d+_/;
-            var imgLink = $(that).find("img")[0].src;
-            var newImgLink = imgLink.replace(regex2, "");
+    $("body").on("mouseenter", ".pic-view.J-pic-view", function () {
+        const that = this;
+        $(".pic-view.J-pic-view").prepend('<button id="openNewImg2"><b>查看大图</b></button>');
+        $("button#openNewImg2").on("click", function () {
+            const regex2 = /s\d+x\d+_/;
+            const imgLink = $(that).find("img")[0].src;
+            const newImgLink = imgLink.replace(regex2, "");
             window.open(newImgLink);
+            return false;
         });
-    })
-    $("body").delegate(".pic-view.J-pic-view", "mouseleave", function () {
+    });
+    $("body").on("mouseleave", ".pic-view.J-pic-view", function () {
         $("button#openNewImg2").remove();
+        return false;
     });
 })();
